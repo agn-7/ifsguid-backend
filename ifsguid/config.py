1328f3bd5582446d1be407b13de0e4ed8f6cbaaa
Benyamin <benyaminjmf@gmail.com>
2023-11-07 12:22:09 +0330

    🗃️ init database

---
+++ ifsguid/config.py
@@ -0,0 +1,32 @@
+from typing import Any, Dict, Optional, Union
+
+from pydantic import BaseSettings, PostgresDsn, validator
+
+
+class Settings(BaseSettings):
+    POSTGRES_SERVER: Optional[str]
+    POSTGRES_USER: Optional[str]
+    POSTGRES_PASSWORD: Optional[str]
+    POSTGRES_DB: Optional[str]
+    SQLALCHEMY_DATABASE_URI: Union[Optional[PostgresDsn], Optional[str]] = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            print("Loading SQLALCHEMY_DATABASE_URI from docker.env file ...")
+            return v
+        print("Creating SQLALCHEMY_DATABASE_URI from .env file ...")
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+
+
+settings = Settings()
---
+++ ifsguid/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from .config import settings
+
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, future=True, bind=engine
+)
