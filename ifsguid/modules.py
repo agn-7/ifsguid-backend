3562ba92afa733fae553a8729e69541d0a9abd5f
Benyamin <benyaminjmf@gmail.com>
2023-11-08 19:36:53 +0330

    ✨ added ai responder

---
+++ ifsguid/modules.py
@@ -0,0 +1,11 @@
+import g4f
+
+g4f.debug.logging = True
+g4f.check_version = False
+
+
+def generate_ai_response(content: str, model: str = "GPT4") -> str:  # TODO: use async version for future
+    return g4f.ChatCompletion.create(
+        model=g4f.models.gpt_4 if model == "GPT4" else g4f.models.gpt_35_turbo,
+        messages=[{"role": "user", "content": content}],
+    )
