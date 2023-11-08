66edbaa3f8fb7201698475a0863e588b491c78f5
Benyamin <benyaminjmf@gmail.com>
2023-11-08 22:58:17 +0330

    🔨 Init bash runner

---
+++ start_app.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eu
+
+host=0.0.0.0
+port=8000
+
+get_absolute_path() {
+  cd "$(dirname "$1")" && pwd -P
+}
+
+cd "$(get_absolute_path "$0")" || exit 1
+
+[[ "${HOST:-}" ]] && host="${HOST}"
+[[ "${PORT:-}" ]] && port="${PORT}"
+poetry run alembic upgrade head
+poetry run uvicorn --host "${host}" --port "${port}" --reload ifsguid.main:app
