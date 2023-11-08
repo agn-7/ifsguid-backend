1e2e924156a038082c42385a2a9dc11195f0f58d
Benyamin <benyaminjmf@gmail.com>
2023-11-08 13:49:40 +0330

    🐛 fixed compatibility with pydantic v2

--- ifsguid/database.py
+++ ifsguid/database.py
@@ -3,7 +3,9 @@ from sqlalchemy.orm import sessionmaker
 
 from .config import settings
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+engine = create_engine(
+    settings.SQLALCHEMY_DATABASE_URI.unicode_string(), pool_pre_ping=True
+)
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, future=True, bind=engine
 )
