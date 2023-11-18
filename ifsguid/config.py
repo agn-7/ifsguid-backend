be1fd5417475d63d08d6363af0cfdfe9c9ecf720
Benyamin <benyaminjmf@gmail.com>
2023-11-18 12:00:19 +0330

    🔧 pydantic v2 migration stuff

--- ifsguid/config.py
+++ ifsguid/config.py
@@ -1,33 +1,32 @@
 from typing import Any, Dict, Optional, Union
 
-from pydantic import PostgresDsn, validator
-from pydantic_settings import BaseSettings
+from pydantic import PostgresDsn, field_validator, ValidationInfo
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", case_sensitive=True)
+
     POSTGRES_SERVER: Optional[str]
     POSTGRES_USER: Optional[str]
     POSTGRES_PASSWORD: Optional[str]
     POSTGRES_DB: Optional[str]
     SQLALCHEMY_DATABASE_URI: Union[Optional[PostgresDsn], Optional[str]] = None
 
-    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
-    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+    @field_validator("SQLALCHEMY_DATABASE_URI", mode="before")
+    @classmethod
+    def assemble_db_connection(cls, v: Optional[str], values: ValidationInfo) -> Any:
         if isinstance(v, str):
-            print("Loading SQLALCHEMY_DATABASE_URI from docker.env file ...")
+            print("Loading SQLALCHEMY_DATABASE_URI from .docker.env file ...")
             return v
         print("Creating SQLALCHEMY_DATABASE_URI from .env file ...")
         return PostgresDsn.build(
             scheme="postgresql",
-            username=values.get("POSTGRES_USER"),
-            password=values.get("POSTGRES_PASSWORD"),
-            host=values.get("POSTGRES_SERVER"),
-            path=f"{values.get('POSTGRES_DB') or ''}",
+            username=values.data.get("POSTGRES_USER"),
+            password=values.data.get("POSTGRES_PASSWORD"),
+            host=values.data.get("POSTGRES_SERVER"),
+            path=f"{values.data.get('POSTGRES_DB') or ''}",
         )
-
-    class Config:
-        env_file = ".env"
-        case_sensitive = True
 
 
 settings = Settings()
