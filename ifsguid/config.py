0c9dc198992edc3056ab7e73cd099db13e044144
Benyamin <benyaminjmf@gmail.com>
2023-11-18 19:56:34 +0330

    🐛 fixed postgres setting validation issue

--- ifsguid/config.py
+++ ifsguid/config.py
@@ -7,10 +7,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", case_sensitive=True)
 
-    POSTGRES_SERVER: Optional[str]
-    POSTGRES_USER: Optional[str]
-    POSTGRES_PASSWORD: Optional[str]
-    POSTGRES_DB: Optional[str]
+    POSTGRES_SERVER: Optional[str] = None
+    POSTGRES_USER: Optional[str] = None
+    POSTGRES_PASSWORD: Optional[str] = None
+    POSTGRES_DB: Optional[str] = None
     SQLALCHEMY_DATABASE_URI: Union[Optional[PostgresDsn], Optional[str]] = None
 
     @field_validator("SQLALCHEMY_DATABASE_URI", mode="before")
