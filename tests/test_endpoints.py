a6bc1f0ae5b5b205c0d674840c534b8d14caeb7e
Benyamin <benyaminjmf@gmail.com>
2023-11-22 00:32:51 +0330

    ✅ 🤡 init integration test

---
+++ tests/test_endpoints.py
@@ -0,0 +1,24 @@
+from ifsguid import models
+from .client import client
+
+
+### Integration Tests ###
+
+
+def test_get_root():
+    response = client.get("/api")
+    assert response.status_code == 200
+    assert response.json() == "Hello from IFSGuid!"
+
+
+def test_get_all_interactions(db):
+    interaction1 = models.Interaction(settings={"prompt": "something"})
+    interaction2 = models.Interaction(settings={"prompt": "something else"})
+    db.add(interaction1)
+    db.add(interaction2)
+    db.commit()
+
+    response = client.get("/api/interactions")
+    assert response.status_code == 200
+    assert len(response.json()) == 2
+
