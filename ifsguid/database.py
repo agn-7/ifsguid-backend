d17e405fc0006c0306e6950edd71757bc299eca2
Benyamin <benyaminjmf@gmail.com>
2023-12-01 17:34:44 +0330

    🏷️ add hinting type

--- ifsguid/database.py
+++ ifsguid/database.py
@@ -1,9 +1,11 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, AsyncEngine
 from sqlalchemy.orm import sessionmaker
 
 from .config import settings
 
-engine = create_async_engine(settings.SQLALCHEMY_DATABASE_URI.unicode_string())
+engine: AsyncEngine = create_async_engine(
+    settings.SQLALCHEMY_DATABASE_URI.unicode_string()
+)
 async_session = sessionmaker(
     autocommit=False,
     autoflush=False,
